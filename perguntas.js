9b4a713a57b62eddda91309b4ee98c6a1a1393a8
Fuolipe <143820336+fuolipe@users.noreply.github.com>
2024-10-11 11:30:34 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Curiosidade de Gatos',
+    'Oque é catnip?',
+    ' é uma espécie de planta herbácea do género Nepeta da família Lamiaceae, nativa da Europa e do sudoeste da Ásia Central e naturalizada muitas regiões temperadas.'
+)
+
+criaCartao(
+    'Curiosidade de Gatos',
+    'para que a catnip serve?',
+    'é super benéfica para os felinos tendo a capacidade de despertar um instinto predador e emoções positivas nos gatos, deixando-os mais energéticos e com vontade de brincar.'
+)
+
+criaCartao(
+    'Curiosidade de Gatos',
+    'Audição dos Gatos',
+    'Um gato consegue ouvir sons de alta frequência, cerca de 2 oitavas acima de qualquer humano.'
+)
+
+criaCartao(
+    'Curiosidade de Gatos',
+    'Por que Gatos são tão curiosos?',
+    'Os gatos têm um forte instinto territorial e têm a tendência de patrulhar e explorar seu território.'
+)
